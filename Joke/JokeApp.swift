a17b0374ec27e4fd87790de357ee24d7bad64414
adilmustafayilmaz <79438419+adilmustafayilmaz@users.noreply.github.com>
2023-07-06 13:20:15 +0300

    Initial Commit

---
+++ Joke/JokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeApp.swift
+//  Joke
+//
+//  Created by Adil Mustafa Yılmaz on 6.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct JokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
